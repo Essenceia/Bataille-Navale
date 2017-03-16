9e24a52ba2c63e6efd9a39a5530bb5f67256cf16
Pooki <julia.desmazes@gmail.com>
2017-03-16 13:54:34 +0100

    Remplacer maintenant tout les systemes cls par clear_window()

---
+++ bin/CMakeFiles/BN.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pooki/Documents/code/c/BatailleNavale/src/Allegro.cpp" "/home/pooki/Documents/code/c/BatailleNavale/bin/CMakeFiles/BN.dir/src/Allegro.cpp.o"
+  "/home/pooki/Documents/code/c/BatailleNavale/src/Bateau.cpp" "/home/pooki/Documents/code/c/BatailleNavale/bin/CMakeFiles/BN.dir/src/Bateau.cpp.o"
+  "/home/pooki/Documents/code/c/BatailleNavale/src/Croiseur.cpp" "/home/pooki/Documents/code/c/BatailleNavale/bin/CMakeFiles/BN.dir/src/Croiseur.cpp.o"
+  "/home/pooki/Documents/code/c/BatailleNavale/src/Cuirasse.cpp" "/home/pooki/Documents/code/c/BatailleNavale/bin/CMakeFiles/BN.dir/src/Cuirasse.cpp.o"
+  "/home/pooki/Documents/code/c/BatailleNavale/src/Destroyer.cpp" "/home/pooki/Documents/code/c/BatailleNavale/bin/CMakeFiles/BN.dir/src/Destroyer.cpp.o"
+  "/home/pooki/Documents/code/c/BatailleNavale/src/Fusee.cpp" "/home/pooki/Documents/code/c/BatailleNavale/bin/CMakeFiles/BN.dir/src/Fusee.cpp.o"
+  "/home/pooki/Documents/code/c/BatailleNavale/src/Menu.cpp" "/home/pooki/Documents/code/c/BatailleNavale/bin/CMakeFiles/BN.dir/src/Menu.cpp.o"
+  "/home/pooki/Documents/code/c/BatailleNavale/src/Partie.cpp" "/home/pooki/Documents/code/c/BatailleNavale/bin/CMakeFiles/BN.dir/src/Partie.cpp.o"
+  "/home/pooki/Documents/code/c/BatailleNavale/src/Sousmarin.cpp" "/home/pooki/Documents/code/c/BatailleNavale/bin/CMakeFiles/BN.dir/src/Sousmarin.cpp.o"
+  "/home/pooki/Documents/code/c/BatailleNavale/src/Tire.cpp" "/home/pooki/Documents/code/c/BatailleNavale/bin/CMakeFiles/BN.dir/src/Tire.cpp.o"
+  "/home/pooki/Documents/code/c/BatailleNavale/src/main.cpp" "/home/pooki/Documents/code/c/BatailleNavale/bin/CMakeFiles/BN.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
